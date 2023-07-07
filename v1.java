3576ae8951d4cfde5d4368cba260a5649ab70716
Nikolas Petroff <130324808+chalkpetro@users.noreply.github.com>
2023-07-07 11:58:51 -0500

    Update v1.java
    Chippin away here lads

--- v1.java
+++ v1.java
@@ -2,5 +2,8 @@ public class v1 {
     public static void main(String[] args) throws Exception 
     {
         System.out.println("Lets see if im getting the hang of pushing files to my repo on git");
+        String str = "It seems that ive retained knowledge. Lets build on it eh?";
+            System.out.println(str);
+            return;
     }
 }
