f2161f697917cf475e4d9b770905996ac13e296f
Nikolas Petroff <130324808+chalkpetro@users.noreply.github.com>
2023-07-07 01:45:27 -0500

    Created v1.java
    Created v1.java to see if I still remember how to push the file to my git repo

---
+++ v1.java
@@ -0,0 +1,6 @@
+public class v1 {
+    public static void main(String[] args) throws Exception 
+    {
+        System.out.println("Lets see if im getting the hang of pushing files to my repo on git");
+    }
+}
