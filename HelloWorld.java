d5c9580a7031ce9f8a6a3b9c6033d0805c579aca
Nikolas Petroff <130324808+chalkpetro@users.noreply.github.com>
2023-07-07 00:01:09 -0500

    7_5_23
    Learning and familiarizing

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World! Java and learning git is going to be a wonderful time. I Look forward to collaborating with other like-minded programmers!");
+    }
+}
