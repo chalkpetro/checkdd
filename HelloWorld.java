fe8483ecb413d84f9e39b29c530fd7de2054de5d
Nikolas Petroff <130324808+chalkpetro@users.noreply.github.com>
2023-07-07 01:41:57 -0500

    Deleted desktop init file
    deleted desktop.init file to see what happens

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! Java and learning git is going to be a wonderful time. I Look forward to collaborating with other like-minded programmers!");
+    public static void main(String[] args) throws Exception 
+    {
+        System.out.println("Hello, lets ride.");
     }
 }
